c51866ecabb8d3e3862f560b473c1d889c1fcedb
Mason Bullock <mason.bullock@ucalgary.ca>
2024-12-03 18:52:05 -0700

    key motor qml link

--- src/PacketFactory/Packets/KeyMotorPacket.h
+++ src/PacketFactory/Packets/KeyMotorPacket.h
@@ -2,8 +2,15 @@
 #define KEYMOTORPACKET_H
 
 #include "../IPacket.h"
+#include "../../Config/PropertyDefinitions.h"
 
 class KeyMotorPacket : public IPacket {
+    Q_OBJECT
+    DEFINE_PROPERTY(unsigned short, MotorSetpoint)
+    DEFINE_PROPERTY(bool, ControlMode)
+    DEFINE_PROPERTY(bool, MotorMode)
+    DEFINE_PROPERTY(bool, SoftwareEnable)
+    DEFINE_PROPERTY(bool, DebugMode)
 public:
     KeyMotorPacket();
     void populatePacket(const QByteArray& data) override;
