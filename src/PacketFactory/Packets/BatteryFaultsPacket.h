96f7438ad1c745b1ba4fcdd7749415881403ccc7
Mason Bullock <mason.bullock@ucalgary.ca>
2024-12-06 17:50:23 -0700

    battery faults qml link

--- src/PacketFactory/Packets/BatteryFaultsPacket.h
+++ src/PacketFactory/Packets/BatteryFaultsPacket.h
@@ -2,50 +2,51 @@
 #define BATTERYFAULTSPACKET_H
 
 #include "../IPacket.h"
+#include "../../Config/PropertyDefinitions.h"
 
 class BatteryFaultsPacket : public IPacket {
+    Q_OBJECT
+    DEFINE_PROPERTY(bool, InternalCommunicationFault)
+    DEFINE_PROPERTY(bool, InternalConverversionFault)
+    DEFINE_PROPERTY(bool, WeakCellFault)
+    DEFINE_PROPERTY(bool, LowCellVoltageFault)
+    DEFINE_PROPERTY(bool, OpenWiringFault)
+    DEFINE_PROPERTY(bool, CurrentSensorFault)
+    DEFINE_PROPERTY(bool, PackVoltageSensorFault)
+    DEFINE_PROPERTY(bool, WeakPackFault)
+    DEFINE_PROPERTY(bool, VoltageRedundancyFault)
+    DEFINE_PROPERTY(bool, FanMonitorFault)
+    DEFINE_PROPERTY(bool, ThermistorFault)
+    DEFINE_PROPERTY(bool, CanbusCommunicationFault)
+    DEFINE_PROPERTY(bool, AlwaysOnSupplyFault)
+    DEFINE_PROPERTY(bool, HighVoltageIsolationFault)
+    DEFINE_PROPERTY(bool, PowerSupply12VFault)
+    DEFINE_PROPERTY(bool, ChargeLimitEnforcementFault)
+    DEFINE_PROPERTY(bool, DischargeLimitEnforcementFault)
+    DEFINE_PROPERTY(bool, ChargerSafetyRelayFault)
+    DEFINE_PROPERTY(bool, InternalMemoryFault)
+    DEFINE_PROPERTY(bool, InternalThermistorFault)
+    DEFINE_PROPERTY(bool, InternalLogicFault)
+
+    DEFINE_PROPERTY(bool, DclReducedDueToLowSoc)
+    DEFINE_PROPERTY(bool, DclReducedDueToHighCellResistance)
+    DEFINE_PROPERTY(bool, DclReducedDueToTemperature)
+    DEFINE_PROPERTY(bool, DclReducedDueToLowCellVoltage)
+    DEFINE_PROPERTY(bool, DclReducedDueToLowPackVoltage)
+    DEFINE_PROPERTY(bool, DclAndCclReducedDueToVoltageFailsafe)
+    DEFINE_PROPERTY(bool, DclAndCclReducedDueToCommunicationFailsafe)
+    DEFINE_PROPERTY(bool, CclReducedDueToHighSoc)
+    DEFINE_PROPERTY(bool, CclReducedDueToHighCellResistance)
+    DEFINE_PROPERTY(bool, CclReducedDueToTemperature)
+    DEFINE_PROPERTY(bool, CclReducedDueToHighCellVoltage)
+    DEFINE_PROPERTY(bool, CclReducedDueToHighPackVoltage)
+    DEFINE_PROPERTY(bool, CclReducedDueToChargerLatch)
+    DEFINE_PROPERTY(bool, CclReducedDueToAlternateCurrentLimit)
+
 public:
     BatteryFaultsPacket();
     void populatePacket(const QByteArray& data) override;
     QJsonObject toJson() override;
-
-private:
-    bool internalCommunicationFault_;
-    bool internalConverversionFault_;
-    bool weakCellFault_;
-    bool lowCellVoltageFault_;
-    bool openWiringFault_;
-    bool currentSensorFault_;
-    bool packVoltageSensorFault_;
-    bool weakPackFault_;
-    bool voltageRedundancyFault_;
-    bool fanMonitorFault_;
-    bool thermistorFault_;
-    bool canbusCommunicationFault_;
-    bool alwaysOnSupplyFault_;
-    bool highVoltageIsolationFault_;
-    bool powerSupply12VFault_;
-    bool chargeLimitEnforcementFault_;
-    bool dischargeLimitEnforcementFault_;
-    bool chargerSafetyRelayFault_;
-    bool internalMemoryFault_;
-    bool internalThermistorFault_;
-    bool internalLogicFault_;
-
-    bool dclReducedDueToLowSoc_;
-    bool dclReducedDueToHighCellResistance_;
-    bool dclReducedDueToTemperature_;
-    bool dclReducedDueToLowCellVoltage_;
-    bool dclReducedDueToLowPackVoltage_;
-    bool dclAndCclReducedDueToVoltageFailsafe_;
-    bool dclAndCclReducedDueToCommunicationFailsafe_;
-    bool cclReducedDueToHighSoc_;
-    bool cclReducedDueToHighCellResistance_;
-    bool cclReducedDueToTemperature_;
-    bool cclReducedDueToHighCellVoltage_;
-    bool cclReducedDueToHighPackVoltage_;
-    bool cclReducedDueToChargerLatch_;
-    bool cclReducedDueToAlternateCurrentLimit_;
 };
 
 #endif // BATTERYFAULTSPACKET_H
