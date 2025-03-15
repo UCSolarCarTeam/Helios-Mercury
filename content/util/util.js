189256533914609b7f0292a71275e6ead0d2f50e
Jeeya Somaiya <jeeyasom@gmail.com>
2025-03-15 12:57:41 -0600

    VSC583 removed unnecessary file

--- content/util/util.js
+++
@@ -1,6 +0,0 @@
-function mapValueToAngle(val) {
-    let minAngle = -135; // Adjust based on your arc
-    let maxAngle = 135;  // Adjust based on your arc
-    let normalizedValue = (val - minValue) / (maxValue - minValue);
-    return minAngle + normalizedValue * (maxAngle - minAngle);
-}
