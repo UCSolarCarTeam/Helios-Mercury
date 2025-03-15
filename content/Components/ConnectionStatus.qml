3ef06dc16659312fff1ec416bb7adbe9c0b9719a
SolarCar <adeebhossain2004@gmail.com>
2025-03-15 11:39:10 -0600

    QML for connection icons

---
+++ content/Components/ConnectionStatus.qml
@@ -0,0 +1,20 @@
+import QtQuick
+import QtQuick.Controls
+import Mercury
+
+Item {
+    id: connectionStatusFrame
+    width: 32
+    height: 32
+
+    property string connection: name
+    property bool isConnected: false
+    property bool isRadioBoard: false
+
+    Image {
+        id: connectionIcon
+        source: isRadioBoard
+            ? (pi.EmbeddedState ? "../Images/RadioBoardConnected.png" : "../Images/RadioBoardDisconnected.png")
+            : (pi.AWSState ? "../Images/TelemetryConnected.png" : "../Images/TelemetryDisconnected.png")
+    }
+}
--- src/Mercury.cpp
+++ src/Mercury.cpp
@@ -57,7 +57,7 @@ Mercury::Mercury(int &argc, char **argv) : QGuiApplication(argc, argv) {
     MpptPacket &mppt3Packet = packetFactory->getMpptPacket(3);
     MbmsPacket &mbmsPacket = packetFactory->getMbmsPacket();
     ProximitySensorsPacket &proximitySensorsPacket = packetFactory->getProximitySensorsPacket();
-    PiPacket &pipacket = packetFactory->getPiPacket();
+    PiPacket &piPacket = packetFactory->getPiPacket();
 
 
     engine_.rootContext()->setContextProperty("keyMotor", &keyMotorPacket);
@@ -73,7 +73,7 @@ Mercury::Mercury(int &argc, char **argv) : QGuiApplication(argc, argv) {
     engine_.rootContext()->setContextProperty("mppt3", &mppt3Packet);
     engine_.rootContext()->setContextProperty("mbms", &mbmsPacket);
     engine_.rootContext()->setContextProperty("proximitySensors", &proximitySensorsPacket);
-    engine_.rootContext()->setContextProperty("pi", &pipacket);
+    engine_.rootContext()->setContextProperty("pi", &piPacket);
 
     qmlRegisterSingletonType(QUrl("qrc:/qt/qml/content/Config/Config.qml"), "Mercury", 1, 0, "Config");
 
