0d5a704462902e075890e2b732755ce81e8b2104
Jeeya Somaiya <jeeyasom@gmail.com>
2025-06-18 13:37:55 -0600

    VSC639 mppt can parsing

--- src/PacketFactory/Packets/MpptPacket.cpp
+++ src/PacketFactory/Packets/MpptPacket.cpp
@@ -12,16 +12,6 @@ namespace {
     const int TEMPERATURE_OFFSET = 8;
 }
 
-MpptPacket::MpptPacket() {
-    setChannelNumber(0);
-    setMpptStatus(0);
-
-    setArrayVoltage(0);
-    setArrayCurrent(0);
-    setBatteryVoltage(0);
-    setTemperature(0);
-}
-
 void MpptPacket::populatePacket(const QByteArray& data) {
     unsigned char statusAndChannelNumber = getValue<unsigned char>(data, STATUS_AND_CHANNEL_NUMBER_OFFSET);
     setMpptStatus(statusAndChannelNumber & MPPT_STATUS_MASK);
@@ -36,12 +26,84 @@ void MpptPacket::populatePacket(const QByteArray& data) {
 QJsonObject MpptPacket::toJson() {
     QJsonObject json;
     
-    json[JsonDefinitions::CHANNEL_NUMBER] = ChannelNumber_;
-    json[JsonDefinitions::IS_ALIVE] = MpptStatus_;
-    json[JsonDefinitions::ARRAY_VOLTAGE] = ArrayVoltage_;
-    json[JsonDefinitions::ARRAY_CURRENT] = ArrayCurrent_;
-    json[JsonDefinitions::BATTERY_VOLTAGE] = BatteryVoltage_;
-    json[JsonDefinitions::TEMPERATURE] = Temperature_;
-
+    // Channel 0 Data (All MPPT units)
+    QJsonObject channel0;
+    channel0[JsonDefinitions::ARRAY_VOLTAGE] = Channel0ArrayVoltage_;
+    channel0[JsonDefinitions::ARRAY_CURRENT] = Channel0ArrayCurrent_;
+    channel0[JsonDefinitions::BATTERY_VOLTAGE] = Channel0BatteryVoltage_;
+    channel0[JsonDefinitions::TEMPERATURE] = Channel0Temperature_;
+    
+    // Channel 1 Data (All MPPT units)
+    QJsonObject channel1;
+    channel1[JsonDefinitions::ARRAY_VOLTAGE] = Channel1ArrayVoltage_;
+    channel1[JsonDefinitions::ARRAY_CURRENT] = Channel1ArrayCurrent_;
+    channel1[JsonDefinitions::BATTERY_VOLTAGE] = Channel1BatteryVoltage_;
+    channel1[JsonDefinitions::TEMPERATURE] = Channel1Temperature_;
+    
+    json["channel0"] = channel0;
+    json["channel1"] = channel1;
+    
     return json;
+}
+
+void MpptPacket::initializeIdActionMap() {
+    qDebug() << "Initializing MPPT Packet ID Action Map";
+    
+    // Channel 0 Messages (All MPPT units)
+    idActionMap[0x600] = [this](QByteArray payload) {  // MPPT0 Channel0
+        setChannel0ArrayVoltage(getValue<quint16>(payload, 0));
+        setChannel0ArrayCurrent(getValue<quint16>(payload, 2));
+        setChannel0BatteryVoltage(getValue<quint16>(payload, 4));
+        setChannel0Temperature(getValue<quint16>(payload, 6));
+    };
+    
+    idActionMap[0x602] = [this](QByteArray payload) {  // MPPT1 Channel0
+        setChannel0ArrayVoltage(getValue<quint16>(payload, 0));
+        setChannel0ArrayCurrent(getValue<quint16>(payload, 2));
+        setChannel0BatteryVoltage(getValue<quint16>(payload, 4));
+        setChannel0Temperature(getValue<quint16>(payload, 6));
+    };
+    
+    idActionMap[0x604] = [this](QByteArray payload) {  // MPPT2 Channel0
+        setChannel0ArrayVoltage(getValue<quint16>(payload, 0));
+        setChannel0ArrayCurrent(getValue<quint16>(payload, 2));
+        setChannel0BatteryVoltage(getValue<quint16>(payload, 4));
+        setChannel0Temperature(getValue<quint16>(payload, 6));
+    };
+    
+    idActionMap[0x606] = [this](QByteArray payload) {  // MPPT3 Channel0
+        setChannel0ArrayVoltage(getValue<quint16>(payload, 0));
+        setChannel0ArrayCurrent(getValue<quint16>(payload, 2));
+        setChannel0BatteryVoltage(getValue<quint16>(payload, 4));
+        setChannel0Temperature(getValue<quint16>(payload, 6));
+    };
+    
+    // Channel 1 Messages (All MPPT units)
+    idActionMap[0x601] = [this](QByteArray payload) {  // MPPT0 Channel1
+        setChannel1ArrayVoltage(getValue<quint16>(payload, 0));
+        setChannel1ArrayCurrent(getValue<quint16>(payload, 2));
+        setChannel1BatteryVoltage(getValue<quint16>(payload, 4));
+        setChannel1Temperature(getValue<quint16>(payload, 6));
+    };
+    
+    idActionMap[0x603] = [this](QByteArray payload) {  // MPPT1 Channel1
+        setChannel1ArrayVoltage(getValue<quint16>(payload, 0));
+        setChannel1ArrayCurrent(getValue<quint16>(payload, 2));
+        setChannel1BatteryVoltage(getValue<quint16>(payload, 4));
+        setChannel1Temperature(getValue<quint16>(payload, 6));
+    };
+    
+    idActionMap[0x605] = [this](QByteArray payload) {  // MPPT2 Channel1
+        setChannel1ArrayVoltage(getValue<quint16>(payload, 0));
+        setChannel1ArrayCurrent(getValue<quint16>(payload, 2));
+        setChannel1BatteryVoltage(getValue<quint16>(payload, 4));
+        setChannel1Temperature(getValue<quint16>(payload, 6));
+    };
+    
+    idActionMap[0x607] = [this](QByteArray payload) {  // MPPT3 Channel1
+        setChannel1ArrayVoltage(getValue<quint16>(payload, 0));
+        setChannel1ArrayCurrent(getValue<quint16>(payload, 2));
+        setChannel1BatteryVoltage(getValue<quint16>(payload, 4));
+        setChannel1Temperature(getValue<quint16>(payload, 6));
+    };
 }
--- src/PacketFactory/Packets/MpptPacket.h
+++ src/PacketFactory/Packets/MpptPacket.h
@@ -6,18 +6,25 @@
 
 class MpptPacket : public IPacket {
     Q_OBJECT
-    DEFINE_PROPERTY(unsigned char, ChannelNumber)
-    DEFINE_PROPERTY(bool, MpptStatus)
-
-    DEFINE_PROPERTY(unsigned short, ArrayVoltage)
-    DEFINE_PROPERTY(unsigned short, ArrayCurrent)
-    DEFINE_PROPERTY(unsigned short, BatteryVoltage)
-    DEFINE_PROPERTY(unsigned short, Temperature)
+    // Channel 0 Properties 
+    DEFINE_PROPERTY(quint16, Channel0ArrayVoltage)
+    DEFINE_PROPERTY(quint16, Channel0ArrayCurrent)
+    DEFINE_PROPERTY(quint16, Channel0BatteryVoltage)
+    DEFINE_PROPERTY(quint16, Channel0Temperature)
     
+    // Channel 1 Properties
+    DEFINE_PROPERTY(quint16, Channel1ArrayVoltage)
+    DEFINE_PROPERTY(quint16, Channel1ArrayCurrent)
+    DEFINE_PROPERTY(quint16, Channel1BatteryVoltage)
+    DEFINE_PROPERTY(quint16, Channel1Temperature)
+
 public:
     MpptPacket();
     void populatePacket(const QByteArray& data) override;
     QJsonObject toJson() override;
+
+protected:
+    void initializeIdActionMap() override;
 };
 
 #endif // MPPTPACKET_H
