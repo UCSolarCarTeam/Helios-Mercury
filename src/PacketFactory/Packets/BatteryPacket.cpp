6c0bb410a0135e19609b2f523700ed10de775492
Jeeya Somaiya <jeeyasom@gmail.com>
2025-06-18 13:36:52 -0600

    VSC639 battery packet can parsing

--- src/PacketFactory/Packets/BatteryPacket.cpp
+++ src/PacketFactory/Packets/BatteryPacket.cpp
@@ -2,47 +2,22 @@
 #include "../../Config/JsonDefinitions.h"
 
 namespace {
-    const int BMU_ALIVE_OFFSET = 1;
-
-    const int BMS_RELAY_STATUS_OFFSET = 2;
     const unsigned char DISCHARGE_RELAY_ENABLED_MASK = 0x01;
     const unsigned char CHARGE_RELAY_ENABLED_MASK = 0x02;
-    const unsigned char CHARGER_SAFETY_RELAY_ENABLED_MASK = 0x04;
+    const unsigned char CHARGER_SAFETY_ENABLED_MASK = 0x04;
     const unsigned char MALFUNCTION_INDICATOR_ACTIVE_MASK = 0x08;
     const unsigned char MULTI_PURPOSE_INPUT_SIGNAL_STATUS_MASK = 0x10;
     const unsigned char ALWAYS_ON_SIGNAL_STATUS_MASK = 0x20;
     const unsigned char IS_READY_SIGNAL_STATUS_MASK = 0x40;
     const unsigned char IS_CHARGING_SIGNAL_STATUS_MASK = 0x80;
-
-    const int POPULATED_CELLS_OFFSET = 3;
-    const int INPUT_12V_OFFSET = 4;
-    const int FAN_VOLTAGE_OFFSET = 8;
-    const int PACK_CURRENT_OFFSET = 12;
-    const int PACK_VOLTAGE_OFFSET = 16;
-    const int PACK_STATE_OF_CHARGE_OFFSET = 20;
-    const int PACK_AMPHOURS_OFFSET = 24;
-    const int PACK_DEPTH_OF_DISCHARGE_OFFSET = 28;
-    const int HIGH_TEMPERATURE_OFFSET = 32;
-    const int HIGH_THERMISTOR_ID_OFFSET = 33;
-    const int LOW_TEMPERATURE_OFFSET = 34;
-    const int LOW_THERMISTOR_ID_OFFSET = 35;
-    const int AVERAGE_TEMPERATURE_OFFSET = 36;
-    const int INTERNAL_TEMPERATURE_OFFSET = 37;
-    const int FAN_SPEED_OFFSET = 38;
-    const int REQUESTED_FAN_SPEED_OFFSET = 39;
-    const int LOW_CELL_VOLTAGE_OFFSET = 40;
-    const int LOW_CELL_VOLTAGE_ID_OFFSET = 42;
-    const int HIGH_CELL_VOLTAGE_OFFSET = 43;
-    const int HIGH_CELL_VOLTAGE_ID_OFFSET = 45;
-    const int AVERAGE_CELL_VOLTAGE_OFFSET = 46;
 }
 
 BatteryPacket::BatteryPacket() {
-    setBmuAlive(false);
+    setBmuAlive(0);
 
     setDischargeRelayEnabled(false);
     setChargeRelayEnabled(false);
-    setChargerSafetyRelayEnabled(false);
+    setChargerSafetyEnabled(false);
     setMalfunctionIndicatorActive(false);
     setMultiPurposeInputSignalStatus(false);
     setAlwaysOnSignalStatus(false);
@@ -50,13 +25,15 @@ BatteryPacket::BatteryPacket() {
     setIsChargingSignalStatus(false);
 
     setPopulatedCells(0);
-    setInput12V(0);
-    setFanVoltage(0);
-    setPackCurrent(0);
-    setPackVoltage(0);
-    setPackStateOfCharge(0);
-    setPackAmphours(0);
-    setPackDepthOfDischarge(0);
+    setInput12V(0.0f);
+    setFanVoltage(0.0f);
+
+    setPackCurrent(0.0f);
+    setPackVoltage(0.0f);
+    setPackStateOfCharge(0.0f);
+    setPackAmphours(0.0f);
+    setPackDepthOfDischarge(0.0f);
+
     setHighTemperature(0);
     setHighThermistorId(0);
     setLowTemperature(0);
@@ -65,11 +42,19 @@ BatteryPacket::BatteryPacket() {
     setInternalTemperature(0);
     setFanSpeed(0);
     setRequestedFanSpeed(0);
+
     setLowCellVoltage(0);
     setLowCellVoltageId(0);
     setHighCellVoltage(0);
     setHighCellVoltageId(0);
     setAverageCellVoltage(0);
+
+    setMaximumCellVoltage(0);
+    setMinimumCellVoltage(0);
+    setMaximumPackVoltage(0);
+    setMinimumPackVoltage(0);
+
+    initializeIdActionMap();
 }
 
 void BatteryPacket::populatePacket(const QByteArray& data) {
@@ -112,61 +97,115 @@ void BatteryPacket::populatePacket(const QByteArray& data) {
 QJsonObject BatteryPacket::toJson() {
     QJsonObject json;
 
+    // BMU Heartbeat
     json[JsonDefinitions::BMU_ALIVE] = BmuAlive_;
+
+    // Startup Info
     json[JsonDefinitions::DISCHARGE_RELAY_ENABLED] = DischargeRelayEnabled_;
     json[JsonDefinitions::CHARGE_RELAY_ENABLED] = ChargeRelayEnabled_;
-    json[JsonDefinitions::CHARGER_SAFETY_ENABLED] = ChargerSafetyRelayEnabled_;
+    json[JsonDefinitions::CHARGER_SAFETY_ENABLED] = ChargerSafetyEnabled_;
     json[JsonDefinitions::MALFUNCTION_INDICATOR_ACTIVE] = MalfunctionIndicatorActive_;
     json[JsonDefinitions::MULTI_PURPOSE_INPUT_SIGNAL_STATUS] = MultiPurposeInputSignalStatus_;
     json[JsonDefinitions::ALWAYS_ON_SIGNAL_STATUS] = AlwaysOnSignalStatus_;
     json[JsonDefinitions::IS_READY_SIGNAL_STATUS] = IsReadySignalStatus_;
     json[JsonDefinitions::IS_CHARGING_SIGNAL_STATUS] = IsChargingSignalStatus_;
+    json[JsonDefinitions::POPULATED_CELLS] = PopulatedCells_;
+    json[JsonDefinitions::INPUT_12V] = Input12V_;
+    json[JsonDefinitions::FAN_VOLTAGE] = FanVoltage_;
+
+    // Pack Info
+    json[JsonDefinitions::PACK_CURRENT] = PackCurrent_;
+    json[JsonDefinitions::PACK_VOLTAGE] = PackVoltage_;
+    json[JsonDefinitions::PACK_STATE_OF_CHARGE] = PackStateOfCharge_;
+    json[JsonDefinitions::PACK_AMPHOURS] = PackAmphours_;
+    json[JsonDefinitions::PACK_DEPTH_OF_DISCHARGE] = PackDepthOfDischarge_;
+
+    // TempInfo
+    json[JsonDefinitions::HIGH_TEMPERATURE] = HighTemperature_;
     json[JsonDefinitions::HIGH_THERMISTOR_ID] = HighThermistorId_;
+    json[JsonDefinitions::LOW_TEMPERATURE] = LowTemperature_;
     json[JsonDefinitions::LOW_THERMISTOR_ID] = LowThermistorId_;
+    json[JsonDefinitions::AVERAGE_TEMPERATURE] = AverageTemperature_;
+    json[JsonDefinitions::INTERNAL_TEMPERATURE] = InternalTemperature_;
+    json[JsonDefinitions::FAN_SPEED] = FanSpeed_;
+    json[JsonDefinitions::REQUESTED_FAN_SPEED] = RequestedFanSpeed_;
 
-    //Battery Cell
-    QJsonObject batteryCell;
+    // Cell Voltages
+    json[JsonDefinitions::LOW_CELL_VOLTAGE] = LowCellVoltage_;
+    json[JsonDefinitions::LOW_CELL_VOLTAGE_ID] = LowCellVoltageId_;
+    json[JsonDefinitions::HIGH_CELL_VOLTAGE] = HighCellVoltage_;
+    json[JsonDefinitions::HIGH_CELL_VOLTAGE_ID] = HighCellVoltageId_;
+    json[JsonDefinitions::AVERAGE_CELL_VOLTAGE] = AverageCellVoltage_;
 
-    batteryCell[JsonDefinitions::POPULATED_CELLS] = PopulatedCells_;
-    batteryCell[JsonDefinitions::LOW_CELL_VOLTAGE] = LowCellVoltage_;
-    batteryCell[JsonDefinitions::LOW_CELL_VOLTAGE_ID] = LowCellVoltageId_;
-    batteryCell[JsonDefinitions::HIGH_CELL_VOLTAGE] = HighCellVoltage_;
-    batteryCell[JsonDefinitions::HIGH_CELL_VOLTAGE_ID] = HighCellVoltageId_;
-    batteryCell[JsonDefinitions::AVERAGE_CELL_VOLTAGE] = AverageCellVoltage_;
-
-    json[JsonDefinitions::BATTERY_CELL] = batteryCell;
-
-    //Battery Fan
-    QJsonObject batteryFan;
-
-    batteryFan[JsonDefinitions::FAN_VOLTAGE] = FanVoltage_;
-    batteryFan[JsonDefinitions::FAN_SPEED] = FanSpeed_;
-    batteryFan[JsonDefinitions::REQUESTED_FAN_SPEED] = RequestedFanSpeed_;
-
-    json[JsonDefinitions::BATTERY_FAN] = batteryFan;
-
-    //Battery Pack
-    QJsonObject batteryPack;
-
-    batteryPack[JsonDefinitions::INPUT_12V] = Input12V_;
-    batteryPack[JsonDefinitions::PACK_CURRENT] = PackCurrent_;
-    batteryPack[JsonDefinitions::PACK_VOLTAGE] = PackVoltage_;
-    batteryPack[JsonDefinitions::PACK_STATE_OF_CHARGE] = PackStateOfCharge_;
-    batteryPack[JsonDefinitions::PACK_AMPHOURS] = PackAmphours_;
-    batteryPack[JsonDefinitions::PACK_DEPTH_OF_DISCHARGE] = PackDepthOfDischarge_;
-
-    json[JsonDefinitions::BATTERY_PACK] = batteryPack;
-
-    //Battery Temperature
-    QJsonObject batteryTemperature;
-
-    batteryTemperature[JsonDefinitions::HIGH_TEMPERATURE] = HighTemperature_;
-    batteryTemperature[JsonDefinitions::LOW_TEMPERATURE] = LowTemperature_;
-    batteryTemperature[JsonDefinitions::AVERAGE_TEMPERATURE] = AverageTemperature_;
-    batteryTemperature[JsonDefinitions::INTERNAL_TEMPERATURE] = InternalTemperature_;
-
-    json[JsonDefinitions::BATTERY_TEMPERATURE] = batteryTemperature;
-
+    // Max Min Voltages
+    json[JsonDefinitions::MAXIMUM_CELL_VOLTAGE] = MaximumCellVoltage_;
+    json[JsonDefinitions::MINIMUM_CELL_VOLTAGE] = MinimumCellVoltage_;
+    json[JsonDefinitions::MAXIMUM_PACK_VOLTAGE] = MaximumPackVoltage_;
+    json[JsonDefinitions::MINIMUM_PACK_VOLTAGE] = MinimumPackVoltage_;
 
     return json;
+}
+
+void BatteryPacket::initializeIdActionMap() {
+    qDebug() << "Initializing Battery Packet ID Action Map";
+
+    // BMU Heartbeat (0x300)
+    idActionMap[0x300] = [this](QByteArray payload) {
+        setBmuAlive(getValue<unsigned char>(payload, 0));
+    };
+
+    // Startup Info (0x301)
+    idActionMap[0x301] = [this](QByteArray payload) {
+        unsigned char relayStatus = getValue<unsigned char>(payload, 0);
+        setDischargeRelayEnabled(relayStatus & DISCHARGE_RELAY_ENABLED_MASK);
+        setChargeRelayEnabled(relayStatus & CHARGE_RELAY_ENABLED_MASK);
+        setChargerSafetyEnabled(relayStatus & CHARGER_SAFETY_ENABLED_MASK);
+        setMalfunctionIndicatorActive(relayStatus & MALFUNCTION_INDICATOR_ACTIVE_MASK);
+        setMultiPurposeInputSignalStatus(relayStatus & MULTI_PURPOSE_INPUT_SIGNAL_STATUS_MASK);
+        setAlwaysOnSignalStatus(relayStatus & ALWAYS_ON_SIGNAL_STATUS_MASK);
+        setIsReadySignalStatus(relayStatus & IS_READY_SIGNAL_STATUS_MASK);
+        setIsChargingSignalStatus(relayStatus & IS_CHARGING_SIGNAL_STATUS_MASK);
+
+        setPopulatedCells(getValue<unsigned char>(payload, 1));
+        setInput12V(getValue<unsigned short>(payload, 2) * 0.1f); // 0.1V
+        setFanVoltage(getValue<unsigned short>(payload, 4) * 0.01f); // 0.01V
+    };
+
+    // Pack Info (0x302)
+    idActionMap[0x302] = [this](QByteArray payload) {
+        setPackCurrent(getValue<short>(payload, 0) * 0.1f); // 0.1A
+        setPackVoltage(getValue<unsigned short>(payload, 2) * 0.1f); // 0.1V
+        setPackStateOfCharge(getValue<unsigned char>(payload, 4) * 0.5f); // 0.5%
+        setPackAmphours(getValue<unsigned short>(payload, 5) * 0.1f); // 0.1Ah
+        setPackDepthOfDischarge(getValue<unsigned char>(payload, 7) * 0.5f); // 0.5%
+    };
+
+    // TempInfo (0x304)
+    idActionMap[0x304] = [this](QByteArray payload) {
+        setHighTemperature(getValue<char>(payload, 0)); // 1°C
+        setHighThermistorId(getValue<unsigned char>(payload, 1));
+        setLowTemperature(getValue<char>(payload, 2)); // 1°C
+        setLowThermistorId(getValue<unsigned char>(payload, 3));
+        setAverageTemperature(getValue<char>(payload, 4)); // 1°C
+        setInternalTemperature(getValue<char>(payload, 5)); // 1°C
+        setFanSpeed(getValue<unsigned char>(payload, 6));
+        setRequestedFanSpeed(getValue<unsigned char>(payload, 7));
+    };
+
+    // Cell Voltages (0x305)
+    idActionMap[0x305] = [this](QByteArray payload) {
+        setLowCellVoltage(getValue<unsigned short>(payload, 0)); // 0.1mV
+        setLowCellVoltageId(getValue<unsigned char>(payload, 2));
+        setHighCellVoltage(getValue<unsigned short>(payload, 3)); // 0.1mV
+        setHighCellVoltageId(getValue<unsigned char>(payload, 5));
+        setAverageCellVoltage(getValue<unsigned short>(payload, 6)); // 0.1mV
+    };
+
+    // Max Min Voltages (0x30A)
+    idActionMap[0x30A] = [this](QByteArray payload) {
+        setMaximumCellVoltage(getValue<unsigned short>(payload, 0));
+        setMinimumCellVoltage(getValue<unsigned short>(payload, 2));
+        setMaximumPackVoltage(getValue<unsigned short>(payload, 4));
+        setMinimumPackVoltage(getValue<unsigned short>(payload, 6));
+    };
 }
--- src/PacketFactory/Packets/BatteryPacket.h
+++ src/PacketFactory/Packets/BatteryPacket.h
@@ -6,25 +6,30 @@
 
 class BatteryPacket : public IPacket {
     Q_OBJECT
+    // BMU Heartbeat (0x300)
     DEFINE_PROPERTY(unsigned char, BmuAlive)
 
+    // Startup Info (0x301)
     DEFINE_PROPERTY(bool, DischargeRelayEnabled)
     DEFINE_PROPERTY(bool, ChargeRelayEnabled)
-    DEFINE_PROPERTY(bool, ChargerSafetyRelayEnabled)
+    DEFINE_PROPERTY(bool, ChargerSafetyEnabled)
     DEFINE_PROPERTY(bool, MalfunctionIndicatorActive)
     DEFINE_PROPERTY(bool, MultiPurposeInputSignalStatus)
     DEFINE_PROPERTY(bool, AlwaysOnSignalStatus)
     DEFINE_PROPERTY(bool, IsReadySignalStatus)
     DEFINE_PROPERTY(bool, IsChargingSignalStatus)
-
     DEFINE_PROPERTY(unsigned char, PopulatedCells)
     DEFINE_PROPERTY(float, Input12V)
     DEFINE_PROPERTY(float, FanVoltage)
+
+    // Pack Info (0x302)
     DEFINE_PROPERTY(float, PackCurrent)
     DEFINE_PROPERTY(float, PackVoltage)
     DEFINE_PROPERTY(float, PackStateOfCharge)
     DEFINE_PROPERTY(float, PackAmphours)
     DEFINE_PROPERTY(float, PackDepthOfDischarge)
+
+    // TempInfo (0x304)
     DEFINE_PROPERTY(unsigned char, HighTemperature)
     DEFINE_PROPERTY(unsigned char, HighThermistorId)
     DEFINE_PROPERTY(unsigned char, LowTemperature)
@@ -34,16 +39,26 @@ class BatteryPacket : public IPacket {
     DEFINE_PROPERTY(unsigned char, FanSpeed)
     DEFINE_PROPERTY(unsigned char, RequestedFanSpeed)
 
+    // Cell Voltages (0x305)
     DEFINE_PROPERTY(unsigned short, LowCellVoltage)
     DEFINE_PROPERTY(unsigned char, LowCellVoltageId)
     DEFINE_PROPERTY(unsigned short, HighCellVoltage)
     DEFINE_PROPERTY(unsigned char, HighCellVoltageId)
     DEFINE_PROPERTY(unsigned short, AverageCellVoltage)
 
+    // Max Min Voltages (0x30A)
+    DEFINE_PROPERTY(unsigned short, MaximumCellVoltage)
+    DEFINE_PROPERTY(unsigned short, MinimumCellVoltage)
+    DEFINE_PROPERTY(unsigned short, MaximumPackVoltage)
+    DEFINE_PROPERTY(unsigned short, MinimumPackVoltage)
+
 public:
     BatteryPacket();
     void populatePacket(const QByteArray& data) override;
     QJsonObject toJson() override;
+
+protected:
+    void initializeIdActionMap() override;
 };
 
 #endif // BATTERYPACKET_H
