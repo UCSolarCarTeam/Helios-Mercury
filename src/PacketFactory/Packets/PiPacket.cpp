e67c86a6033f5d89fc0de15fb853b8bdcb001a02
Mason Bullock <mason.bullock@ucalgary.ca>
2025-08-19 15:29:59 -0600

    comments and naming correction

--- src/PacketFactory/Packets/PiPacket.cpp
+++ src/PacketFactory/Packets/PiPacket.cpp
@@ -6,10 +6,15 @@ PiPacket::PiPacket() {
     setRfid(NULL);
     setDriverName("Unknown");
     setLatency(-69);
-    setisTelemetryConnected(false);
+    setIsTelemetryConnected(false);
     setIp(findIp());
 }
 
+/**
+    Finds the ip used by the pi to ease connections when debugging
+
+    Looks for the interface that uses Ethernet and is ipv4 protocol and returns the first match
+*/
 QString PiPacket::findIp() const {
     foreach(const QNetworkInterface &interface, QNetworkInterface::allInterfaces()){
         if(interface.type() == QNetworkInterface::Ethernet){
